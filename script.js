81d2e4e3520569b67833e9a7f08325f0cf979898
hetvikanani <hetvipatel3110@gmail.com>
2022-12-14 19:05:52 +0530

    complete movie app

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const APIURL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 
-const main = document.querySelector("main")
+const main = document.querySelector("main");
+const form = document.getElementById('form');
+const searchIn = document.getElementById('search');
 
-const movieData = async () => {
-    const res = await fetch(APIURL);
-    const resData = await res.json();
-    // console.log(resData.results);
-    // return resData;
+const showMovies = (resData) => {
+
+    main.innerHTML = ''
 
     resData.results.forEach((data) => {
-        // console.log(data);
 
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
@@ -23,24 +24,43 @@ const movieData = async () => {
                 <h3>${data.title}</h3>
                 <span class=${rating(data.vote_average)}>${data.vote_average}</span>
             </div>
+            <div class="overview">
+            <h3>Overview:</h3>
+            ${data.overview}
+            </div>
         `;
 
         main.append(movieEl);
     });
+}
+
+const movieData = async (URL) => {
+    const res = await fetch(URL);
+    const resData = await res.json();
+    showMovies(resData)
 };
 
 
 const rating = (vote) => {
     if (vote >= 8) {
         return "blue"
-    }
-    else if (vote >= 5) {
+    } else if (vote >= 5) {
         return "red"
-    }
-    else {
+    } else {
         return "yellow"
     }
 }
 
 
-movieData();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchVal = searchIn.value;
+    if (searchVal) {
+        movieData(SEARCHAPI + searchVal);
+        searchIn.value = '';
+    }
+})
+
+
+movieData(APIURL);
+
